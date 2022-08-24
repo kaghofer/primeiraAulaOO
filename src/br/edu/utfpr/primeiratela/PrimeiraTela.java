49fb7af077a87352be607f3b6831d7bba2987b7a
Daniel Kaghoer <kaghofer@gmail.com>
2022-08-24 20:13:53 -0300

    Com dados
    Atualizacao

--- src/br/edu/utfpr/primeiratela/PrimeiraTela.java
+++ src/br/edu/utfpr/primeiratela/PrimeiraTela.java
@@ -22,30 +22,45 @@ import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.text.MaskFormatter;
 
-public class PrimeiraTela {
+public class PrimeiraTela extends JFrame{
 
     private JTextField txtNome;
     private JTextArea taDados;
+    private final JButton btnSalvar;
+    private final JButton btnCancelar;
+    private final JScrollPane apDados;
+    private JLabel lbNome;
+    private JCheckBox cbJava;
+    private JCheckBox cbDelphi;
+    private JLabel lbGenero;
+    private JRadioButton rbMasc;
+    private JRadioButton rbFem;
+    private ButtonGroup bgGenero;
+    private JLabel lbLivros;
+    private JLabel lbCpf;
+    private JFormattedTextField ftCPF;
+    
+    
     public PrimeiraTela() {
         JFrame frame = new JFrame("Primeira Tela");
 
-        JLabel lbNome = new JLabel("Nome");
+        lbNome = new JLabel("Nome");
         txtNome = new JTextField();
         txtNome.setColumns(10);
-        JLabel lbGenero = new JLabel("Gênero");
+        lbGenero = new JLabel("Gênero");
 
-        JRadioButton rbMasc = new JRadioButton("Masculino");
-        JRadioButton rbFem = new JRadioButton("Feminino");
+        rbMasc = new JRadioButton("Masculino");
+        rbFem = new JRadioButton("Feminino");
 
-        ButtonGroup bgGenero = new ButtonGroup();
+        bgGenero = new ButtonGroup();
         bgGenero.add(rbFem);
         bgGenero.add(rbMasc);
 
-        JLabel lbLivros = new JLabel("Livros");
-        JCheckBox cbJava = new JCheckBox("Java");
-        JCheckBox cbDelphi = new JCheckBox("Delphi");
-        JLabel lbCpf = new JLabel("CPF");
-        JFormattedTextField ftCPF = null;
+        lbLivros = new JLabel("Livros");
+        cbJava = new JCheckBox("Java");
+        cbDelphi = new JCheckBox("Delphi");
+        lbCpf = new JLabel("CPF");
+        ftCPF = null;
         try {
             MaskFormatter mf = new MaskFormatter("###.###.###-##");
             ftCPF = new JFormattedTextField(mf);
@@ -70,10 +85,10 @@ public class PrimeiraTela {
         pnDados.add(lbCpf);
         pnDados.add(ftCPF);
 
-        JButton btnSalvar = new JButton("Salvar");
-        JButton btnCancelar = new JButton("Cancelar");
+         btnSalvar = new JButton("Salvar");
+         btnCancelar = new JButton("Cancelar");
         taDados = new JTextArea();
-        JScrollPane apDados = new JScrollPane(taDados);
+        apDados = new JScrollPane(taDados);
 
         JPanel pnBotoes = new JPanel();
         pnBotoes.add(btnSalvar);
@@ -89,14 +104,49 @@ public class PrimeiraTela {
 
         btnSalvar.addActionListener(e -> {
             
-            taDados.setText(txtNome.getText());
+            taDados.setText(getDados());
         });
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 500);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    
+    }
+    
+     private String getGenero(){
+        String genero = null;
+        if(rbMasc.isSelected())
+        {
+            genero = rbMasc.getActionCommand();
+        }else if(rbFem.isSelected())
+        {
+            genero = rbFem.getActionCommand();
+        }
+        return genero;
+    }
+    
+    public String getLivros()
+    {
+        String livros = "";
+        if(cbJava.isSelected())
+        {
+            livros = cbJava.getActionCommand() + "\n";
+        }if(cbDelphi.isSelected())
+        {
+            livros+=cbDelphi.getActionCommand() + "\n";
+        }
+        return livros;
+    }
 
+    private String getDados()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(txtNome.getText()).append("\n")
+                .append(getGenero()).append("\n")
+                .append(getLivros())
+                .append(ftCPF.getText());
+        return sb.toString();        
     }
 
     public static void main(String[] args) {
